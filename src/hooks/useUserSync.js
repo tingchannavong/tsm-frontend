d0f76d450f66c5795b331a46a6720adbe5ffca9e
tingchannavong <tingchannavong.gmail.com>
2026-06-29 22:13:09 +0700

    refactor: clean up unused code and comments in useUserSync hook

--- src/hooks/useUserSync.js
+++ src/hooks/useUserSync.js
@@ -8,27 +8,8 @@ import * as userService from "../api/user.js";
 
 export const useUserSync = () => {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  // const accessToken = useAuthStore((state) => state.accessToken);
-  // const logout = useAuthStore((state) => state.logout);
-
-  // const setUser = useUserStore((state) => state.setUser);
   const clearUser = useUserStore((state) => state.clearUser);
   const syncUser = useUserStore((state) => state.syncUser);
-
-    // const syncUser = async () => {
-    //   if (!accessToken || isTokenExpired(accessToken)) {
-    //     logout();
-    //     return;
-    //   }
-
-    //   try {
-    //     const userData = await userService.fetchMe();
-    //     setUser(userData);
-    //   } catch (error) {
-    //     console.error("Fetch error:", error);
-    //     toast.error(error?.response?.data?.message || "Failed to sync user");
-    //   }
-    // };
 
   useEffect(() => {
     if (isAuthenticated) {

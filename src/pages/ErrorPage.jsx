fae744d8669aa7a84ac24028249c2b77ce3fb1b9
tingchannavong <tingchannavong.gmail.com>
2026-06-30 21:14:05 +0700

    debug line added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,11 @@
 import styles from "../styles/LoginPage.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useLocation } from "react-router";
 
 function ErrorPage() {
+  const location = useLocation();
+  console.log('Failed at location.pathname', location.pathname);
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
